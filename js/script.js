dc2d8211d13150ca0bf3cd7adc0dfc2ba299a204
NathanNunesCalsonari <rm552539@fiap.com.br>
2023-05-03 22:56:01 -0300

    Button-Rgb com addEventListener Math getById.

--- js/script.js
+++ js/script.js
@@ -13,3 +13,32 @@
 
 //console.log(nome1);
 
+
+let botao = document.getElementById("meu-btn");
+//console.log(botao.textContent);
+
+
+botao.addEventListener("click", function(){ 
+    // console.log(botao.textContent);
+    //Função matemática Math
+    //random  = Retorna um número aleatório entre 0 e 1 Ex: 0.2282632765;
+    //floor = Ela arredonda um número para baixo
+    // ceil = Ela arredonda um número para cima
+    //round = Ela arredonda um número para aleatoriamente.
+
+    let r=0,g=0,b=0;
+    r = Math.round(Math.random()*255);
+    g = Math.round(Math.random()*255);
+    b = Math.round(Math.random()*255);
+    
+    //this.setAttribute("style", "background-color:rgb("+r+","+g+","+b+");");
+this.setAttribute("style", `background-color:rgb(${r},${g},${b});`);
+
+} );
+
+
+//ESTUDAR
+//querySelector
+//querySelectorAll
+//Array
+//Estruturas de repetição
